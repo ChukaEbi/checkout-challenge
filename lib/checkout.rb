600a0d67324c0870950b7347552c4d93152ae0a1
Chuka Ebi <chuka.ebi@outlook.com>
2016-01-22 16:20:20 +0000

    can add items to basket

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,3 +1,23 @@
+require 'products'
+require 'promotional_rules'
+
 class Checkout
 
+  attr_reader :basket, :promotional_rules
+
+  def initialize(promotional_rules = PromotionalRules.new, products = Products.new)
+    @basket = []
+    @promotional_rules = promotional_rules
+    @products = products
+  end
+
+
+  def items_available
+    @products.items
+  end
+
+  def scan(item)
+    item_scanned = items_available.fetch(item);
+    @basket.push(item_scanned);
+  end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,7 +2,7 @@ require 'checkout'
 
 describe Checkout do
 
-  let(:products) { double(:products, items => {001 => ["Lavender heart", 9.25],
+  let(:products) { double(:products, :items => {001 => ["Lavender heart", 9.25],
                                       002 => ["Personalised cufflinks", 45.00],
                                       003 => ["Kids T-shirt", 19.95]}) }
   let (:promotional_rules) { double(:promotional_rules) }
@@ -11,6 +11,11 @@ describe Checkout do
   subject(:checkout) {described_class.new}
 
   context "when I want to  buy an item" do
+
+    it 'can see the items available for purchase' do
+      expect(checkout.items_available).to eq(products.items)
+    end
+
     describe "#scan" do
       it "adds an item to the basket" do
         checkout.scan(1)
